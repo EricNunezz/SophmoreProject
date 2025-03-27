1e40409ab4f6262b2c51860b1c94b77ffe999164
EricNunez <ericnunez2655@gmail.com>
2025-03-26 21:19:17 -0400

    Used Vite to set up my project

---
+++ SmartFit/src/App.tsx
@@ -0,0 +1,14 @@
+import { Button } from "@/components/ui/button"
+
+function App() {
+
+
+  const x = 20;
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen">
+      <Button>Start Quiz {x}</Button>
+    </div>
+  )
+}
+
+export default App
